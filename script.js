d72fdb82c15e458f195522b81c6457c3b00dc3b3
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-08-11 19:55:48 -0300

    Javascript do requisito dezoito e três

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const btnLogin = document.querySelector('#login');
+btnLogin.addEventListener('click', () => {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+const btnSubmit = document.querySelector('#submit-btn');
+const term = document.querySelector('#agreement');
+term.addEventListener('change', () => {
+  if (term.checked === true) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+});
+
 const textarea = document.querySelector('textarea');
 const count = document.getElementById('counter');
 function counter(e) {
